129fff1d3fa5389190353dcb3c2e01d50767257c
Xiaoxing Ye <ye@xiaoxing.us>
2016-08-04 02:11:39 +0800

    add comments

--- backends/yourmembership.py
+++ backends/yourmembership.py
@@ -27,6 +27,9 @@ class YourMembershipAuth(BaseAuth):
     YMAPI_VERSION = "2.03"
 
     def generate_request_xml(self, _call_method, _session_id="", _call_args={}):
+        """
+        Generate the request xml data.
+        """
         API_KEY, PRIVATE_KEY = self.get_key_and_secret()
         top = Element('YourMembership')
 
@@ -53,34 +56,23 @@ class YourMembershipAuth(BaseAuth):
         return parseString(ElementTree.tostring(top)).toprettyxml()
 
     def call_api(self, _call_method, _session_id="", _call_args={}):
-        data = self.generate_request_xml(_call_method, _session_id=_session_id, _call_args=_call_args)
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'} 
+        """Call YourMembership API."""
+        data = self.generate_request_xml(_call_method, _session_id=_session_id, _call_args=_call_args)  # Get the data to post
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}   # Use this content type
     
-        response = request("POST", self.YMAPI_SERVLET_URI, headers=headers, data=data)
-        self.procees_error(response)
-        parse = self.xml_parse(response)
-        ym_response = parse.findall("./" + _call_method)[0]
-        return_dict = {"call_method": _call_method}
-        for node in ym_response:
-            return_dict[node.tag] = node.text
-        return return_dict
-
-    def extra_data(self, user, uid, response, details=None, *args, **kwargs):
-        """Return access_token and extra defined names to store in
-        extra_data field"""
-        data = super(YourMembershipAuth, self).extra_data(user, uid, response, details,
-                                                 *args, **kwargs)
-        data['access_token'] = response.get('access_token', '') or \
-                               kwargs.get('access_token')
-        return data
-
-    def procees_error(self, response):
-        xmldoc = self.xml_parse(response)
+        response = request("POST", self.YMAPI_SERVLET_URI, headers=headers, data=data)  # Send the request
+        
+        xmldoc = self.ElementTree.fromstring(response.text)
         errcode = int(xmldoc.findall("./ErrCode")[0].text)
         ExtendedErrorInfo = xmldoc.findall("./ExtendedErrorInfo")
         errdesc = xmldoc.findall("./ErrDesc")
+
         if errcode == 0:
-            return
+            ym_response = xmldoc.findall("./" + _call_method)[0]
+            return_dict = {"call_method": _call_method}
+            for node in ym_response:
+                return_dict[node.tag] = node.text
+            return return_dict
         elif errcode == 999:
             raise AuthUnknownError(self, errdesc[0].text)
         elif errcode in [101, 102, 103, 201, 301, 404]:
@@ -89,23 +81,21 @@ class YourMembershipAuth(BaseAuth):
             raise AuthStateForbidden(self, errdesc[0].text)
         else:
             raise AuthUnknownError(self, errdesc[0].text)
+        
 
     def create_session(self):
+        """ Create a new API session."""
         return self.call_api("Session.Create")["SessionID"]
 
     def auth_url(self):
         """Return redirect url"""
         sessionID = self.create_session()
-        self.strategy.session_set("ymsessionID", sessionID)
-        data = self.call_api("Auth.CreateToken", sessionID, _call_args={"RetUrl": self.get_redirect_uri()})
+        self.strategy.session_set("ymsessionID", sessionID)  # store the sessionID in this browser session.
+        data = self.call_api("Auth.CreateToken", sessionID, _call_args={"RetUrl": self.get_redirect_uri()})  # grab the GoToUrl
         return data["GoToUrl"]
 
-    @staticmethod
-    def xml_parse(response):
-        return ElementTree.fromstring(response.text)
-
     def get_redirect_uri(self):
-        """Build redirect with redirect_state parameter."""
+        """Build redirect uri."""
         uri = self.redirect_uri
         return uri
 
@@ -117,7 +107,7 @@ class YourMembershipAuth(BaseAuth):
         if not sessionID:
             raise AuthStateMissing(self, "Missing Session ID.")
 
-        response = self.call_api("Member.Profile.Get", sessionID)
+        response = self.call_api("Member.Profile.Get", sessionID)  # fetch user data
         kwargs.update({'response': response, 'backend': self})
         return self.strategy.authenticate(*args, **kwargs)
 
